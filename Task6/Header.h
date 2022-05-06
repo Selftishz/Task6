7d7c573f0d966d43702087919bc8639a73d898d2
Artyom <Artyom@DESKTOP-HB4QEA4>
2022-05-06 22:05:20 +0300

    Добавьте файлы проекта.

---
+++ Task6/Header.h
@@ -0,0 +1,16 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+#include <iostream>
+
+using namespace std;
+
+bool InCheck(int num);
+void FillArr(int* Arr, int n);
+void FillArr(double* Arr, int n);
+void OutArr(int* arr, int n);
+void OutArr(double* arr, int n);
+void bubbleSort(int* Arr, int n);
+void bubbleSort(double* Arr, int n);
+
+#endif 
---
+++ Task6/main.cpp
@@ -0,0 +1,30 @@
+﻿#include "Header.h"
+
+int main()
+{
+	srand(time(0));
+	cout << "Write count of random elements: ";
+	int n;
+	cin >> n;
+
+	if (!InCheck(n)) return 0;
+
+	int* Arr = new int[n];
+	FillArr(Arr, n);
+	cout << "Int array: \n";
+	OutArr(Arr, n);
+	cout << "Sorted array: \n";
+	bubbleSort(Arr, n);
+
+	OutArr(Arr, n);
+
+	delete[] Arr;
+	double* doubleArr = new double[n];
+	FillArr(doubleArr, n);
+	cout << "Double array: \n";
+	OutArr(doubleArr, n);
+	cout << "Sorted array: \n";
+	bubbleSort(doubleArr, n);
+	OutArr(doubleArr, n);
+	delete[] doubleArr;
+}
